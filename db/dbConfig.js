f368647a7854fdf31f54067acff8a32d38b9ee93
Chantal <chantalgomez@pursuit.org>
2024-02-01 16:19:31 -0500

    changing db config, different lay out

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,7 +1,12 @@
 require("dotenv").config();
 const pgp = require("pg-promise")();
 
-const cn = {
+ const { DATABASE_URL } = process.env;
+
+const cn = DATABASE_URL ? {
+  connectionString: DATABASE_URL,
+  max: 30
+} : {
   host: process.env.PG_HOST,
   port: process.env.PG_PORT,
   database: process.env.PG_DATABASE,
@@ -9,13 +14,15 @@ const cn = {
   password: process.env.PG_PASSWORD,
 };
 const db = pgp(cn);
-db.connect()
-  .then((obj) => {
-    const serverVersion = obj.client.serverVersion;
+// db.connect()
+//   .then((obj) => {
+//     const serverVersion = obj.client.serverVersion;
+//     console.log("postgres connection established");
+//     obj.done();
+//   })
+//   .catch((error) => {
+//     console.log("ERROR", error.message || error);
+//   });
     console.log("postgres connection established");
-    obj.done();
-  })
-  .catch((error) => {
-    console.log("ERROR", error.message || error);
-  });
+
 module.exports = db;
