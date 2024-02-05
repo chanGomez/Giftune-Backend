1d654e20cb3164f5eb18649383c1dd96abd73fe7
Chantal <chantalgomez@pursuit.org>
2024-02-05 12:47:10 -0500

    changing config file for a different style

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,28 +1,20 @@
 require("dotenv").config();
 const pgp = require("pg-promise")();
-
- const { DATABASE_URL } = process.env;
-
-const cn = DATABASE_URL ? {
-  connectionString: DATABASE_URL,
-  max: 30
-} : {
+const cn = {
   host: process.env.PG_HOST,
   port: process.env.PG_PORT,
   database: process.env.PG_DATABASE,
   user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD
+  password: process.env.PG_PASSWORD,
 };
 const db = pgp(cn);
-// db.connect()
-//   .then((obj) => {
-//     const serverVersion = obj.client.serverVersion;
-//     console.log("postgres connection established");
-//     obj.done();
-//   })
-//   .catch((error) => {
-//     console.log("ERROR", error.message || error);
-//   });
+db.connect()
+  .then((obj) => {
+    const serverVersion = obj.client.serverVersion;
     console.log("postgres connection established");
-
+    obj.done();
+  })
+  .catch((error) => {
+    console.log("ERROR", error.message || error);
+  });
 module.exports = db;
